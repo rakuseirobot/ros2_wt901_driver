765dad19dbbd3cbfc0b32a604171031aa75f8c49
Soula <helloworldwithsoulas@gmail.com>
2020-06-28 11:12:56 +0900

    added new publisher for magnetic field val

--- src/wt901_driver/src/converter.cpp
+++ src/wt901_driver/src/converter.cpp
@@ -3,6 +3,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "serial_message/msg/serial.hpp"
 #include "sensor_msgs/msg/imu.hpp"
+#include "sensor_msgs/msg/magnetic_field.hpp"
 
 using std::placeholders::_1;
 
@@ -12,20 +13,24 @@ public:
 private:
     inline void Convert(const serial_message::msg::Serial::SharedPtr data);
     rclcpp::Subscription<serial_message::msg::Serial>::SharedPtr input_;
-    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr output_;
+    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_output_;
+    rclcpp::Publisher<sensor_msgs::msg::MagneticField>::SharedPtr magnetic_output_;
 };
 
 Converter::Converter() : Node("imu_converter"){
-    std::string input_topic, output_topic;
+    std::string input_topic, imu_out, magnetic_out;
 
     this->declare_parameter<std::string>("Input_Topic", "RxSerial1");
-    this->declare_parameter<std::string>("Output_Topic", "IMU1");
+    this->declare_parameter<std::string>("IMU_Out", "IMU1");
+    this->declare_parameter<std::string>("Magnetic_Out", "Magnetic1");
 
     this->get_parameter<std::string>("Input_Topic", input_topic);
-    this->get_parameter<std::string>("Output_Topic", output_topic);
+    this->get_parameter<std::string>("IMU_Out", imu_out);
+    this->get_parameter<std::string>("Magnetic_Out", magnetic_out);
 
     input_ = this->create_subscription<serial_message::msg::Serial>(input_topic, 10, std::bind(&Converter::Convert, this, _1));
-    output_ = this->create_publisher<sensor_msgs::msg::Imu>(output_topic, 10);
+    imu_output_ = this->create_publisher<sensor_msgs::msg::Imu>(imu_out, 10);
+    magnetic_output_ = this->create_publisher<sensor_msgs::msg::MagneticField>(magnetic_out, 10);
 }
 
 void Converter::Convert(const serial_message::msg::Serial::SharedPtr data){
